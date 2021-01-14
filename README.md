5255503bf20635a28308380b6d14c59a7d439be8
Marshall Haymond <Marshallhay@msn.com>
2021-01-14 15:18:06 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Overview
+
+
+
+IN THE WORKS
+
+
+Watch a sample video here:
+
+# Development Environment
+
+
+# Overview
+
+
+
+This is a Hello World program. I made this to show how to to VScode, Github, and Python
+
+
+Watch a sample video here:
+
+# Development Environment
+
+* VScode
+* Python 3.7.8
+* Git /GitHub
+
+
+# Useful Websites
+
+
---
+++ main.py
@@ -0,0 +1,191 @@
+import arcade
+import random
+import os
+
+import arcade
+
+SPRITE_SCALING = 0.4
+TILE_SCALING = 0.5
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = "Rexburg racing"
+
+
+MOVEMENT_SPEED = 30
+GRAVITY = .001
+
+
+
+
+class Car(arcade.Sprite):
+    """ Player Class """
+
+    def update(self):
+        """ Move the player """
+        # Move player.
+        # Remove these lines if physics engine is moving player.
+        #self.center_x += self.change_x
+        #self.center_y += self.change_y
+
+        # Check for out-of-bounds
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self, width, height, title):
+        """
+        Initializer
+        """
+
+        # Call the parent class initializer
+        super().__init__(width, height, title)
+
+        self.coin_list = None
+        self.tree_list = None
+        self.player_list = None
+        self.road_list = None
+
+        self.physics_engine = None
+
+        # Variables that will hold sprite lists
+        self.player_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.LIGHT_BLUE)
+
+
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        
+        # Create the Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.tree_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+        self.road_list = arcade.SpriteList()
+
+        # Set up the player
+        self.player_sprite = Car("car1.png", SPRITE_SCALING)
+        self.player_sprite.center_x = 100
+        self.player_sprite.center_y = 150
+        self.player_list.append(self.player_sprite)
+
+            # --- Load in a map from the tiled editor ---
+
+        # Name of map file to load
+        map_name = "background.tmx"
+        # Name of the layer in the file that has our platforms/walls
+        platforms_layer_name = 'Tree_plat'
+        # Name of the layer that has items for pick-up
+        coins_layer_name = 'Coins_plat'
+
+        road_layer_name = 'Road_plat'
+
+        # Read in the tiled map
+        my_map = arcade.tilemap.read_tmx(map_name)
+
+        # -- Platforms
+        self.tree_list = arcade.tilemap.process_layer(map_object=my_map,
+                                                      layer_name=platforms_layer_name,
+                                                      scaling=TILE_SCALING,
+                                                      use_spatial_hash=True)
+
+        # -- Coins
+        self.coin_list = arcade.tilemap.process_layer(my_map, coins_layer_name, TILE_SCALING)
+
+        # --- Other stuff
+        self.road_list = arcade.tilemap.process_layer(my_map,road_layer_name, TILE_SCALING )
+        # Set the background color
+        if my_map.background_color:
+            arcade.set_background_color(my_map.background_color)
+
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
+                                                             self.tree_list,
+                                                             GRAVITY)
+
+
+
+
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw all the sprites. Make sure you place background frist
+        self.road_list.draw()
+        self.tree_list.draw()
+        self.coin_list.draw()
+        self.player_list.draw()
+
+        
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        self.physics_engine.update()
+
+        self.player_list.update()
+        coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                             self.coin_list)
+        for coin in coin_hit_list:
+            # Remove the coin
+            coin.remove_from_sprite_lists()                                                     
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        # If the player presses a key, update the speed
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        # If a player releases a key, zero out the speed.
+        # This doesn't work well if multiple keys are pressed.
+        # Use 'better move by keyboard' example if you need to
+        # handle this.
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
+
+
+def main():
+    """ Main method """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
